ef619d87d681603b87f533a34b06e57b7439cd68
Xyvreal <oliveira_samuel.z@hotmail.com>
2023-05-03 13:57:20 -0600

    Prep1 Assignment. Extra function added in 'Trim();'

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+        Console.WriteLine("What is your first name? ");
+        string fName = Console.ReadLine().Trim();
+        Console.WriteLine("What is your last name? ");
+        string lName = Console.ReadLine().Trim();
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
+
+    } 
 }

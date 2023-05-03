542a41b41a076fb958e273272dd3efde26e35347
Xyvreal <oliveira_samuel.z@hotmail.com>
2023-05-03 14:50:12 -0600

    Learning02 resume lesson

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Waffle Flipper";
+        job1._company = "Tesla Motors";
+        job1._startYear = 5469;
+        job1._endYear = 6773;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Crisis Coordinator";
+        job2._company = "Chaos Inc.";
+        job2._startYear = 6772;
+        job2._endYear = 7865;
+
+        Resume resOne = new Resume();
+        resOne._name = "Sammich the Rhetorical";
+        resOne._jobs.Add(job1);
+        resOne._jobs.Add(job2);
+
+        resOne.Display();
     }
 }
